51baacb9a77e5b0850a2f010403a6610937ebafe
GKalmus <kalmusgert@hotmail.com>
2025-05-15 23:54:11 +0300

    Updated nvim colors

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("config.lazy")
+require("config.tree")
 require("config.core")
 require("config.keys")
-require("config.tree")
--- config/nvim/lua/config/core.lua
+++ config/nvim/lua/config/core.lua
@@ -1,22 +1,20 @@
-vim.opt.number = true
+vim.opt.swapfile = false
 
+vim.opt.number = true
+vim.opt.wrap = false
+
+vim.opt.smartindent = true
+vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Colors
+vim.opt.termguicolors = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.cmd.colorscheme("vesper")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "Comment", { ctermfg = "gray" })
-vim.api.nvim_set_hl(0, "NeoTreeMessage", { ctermfg = "gray" })
-
-vim.opt.wrap = false
-vim.opt.smartindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.termguicolors = false
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.scrolloff = 8
-
-vim.opt.swapfile = false
-
